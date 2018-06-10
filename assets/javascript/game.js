4797b4a93afcf8f2be4dc607acde857f861cbf12
AR123456 <coloradoz99@gmail.com>
2018-06-10 12:23:00 -0600

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,3 @@
-// document . ready function
-
-// Variables   wins, losses, array of words, array of letters in word
-
-//functions   math.random on word array to find new word,  loop the found word create an underscore for each letter ,
-
-//main process or game
-// on key event listoner to start game , when clicked generate the new workd
-
-// display the underscores for the word
-
-//display already guessed letters
-
-//display correct letters in the place of the underscores in the word
-
-//at end of game automacitaly start new game
-// <!-- the styling of the game  -->
-// <!--  if the correct letter is guessed make a swish sound   -->
-// <!--  if  in correct letter is guessed make a horse wininie sound  -->
-// <!--  at end of game  update wins and losses -->
-// <!--  for game one  do a celibration annimation and play a song , if lost play hourse sounds and a picure of a horse   -->
-
-// <!-- then back to main game background  -->
-
 // Variables
 
 let wordList = [
@@ -42,8 +18,8 @@ let underscoreWord = [];
 let badGuess = [];
 let horse = document.createElement("img"); // horse image for loss
 let celebrate = document.createElement("img"); // image for win
-let winCount = "";
-let lossCount = "";
+let winCount = 0;
+let lossCount = 0;
 let guessesLeft = 5;
 
 //functions
@@ -117,7 +93,7 @@ function roundComplete() {
     // document.getElementById("face").appendChild(emoji);
     document.getElementById("winCounter").innerHTML = winCount;
     setTimeout(function() {
-      alert("You Won ! The word is: ", currentWord);
+      alert("You Won ! The word is: " + currentWord);
       startGame();
     }, 1000);
   } else if (guessesLeft == 0) {
@@ -127,8 +103,10 @@ function roundComplete() {
     // emoji.setAttribute("style", "z-index: 1;");
     // document.getElementById("face").appendChild(emoji);
 
-    alert("You lost! The correct word was: ", currentWord);
+    alert("You lost! The correct word was: " + currentWord);
     document.getElementById("lossCount").innerHTML = lossCount;
+    document.getElementById("wrongGuesses").innerHTML = "  ";
+
     startGame();
   }
 }
