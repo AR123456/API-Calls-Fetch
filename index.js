e3d646782794335d0fd48fa70c26a91cf9d594a3
AR123456 <coloradoz99@gmail.com>
2019-12-22 07:00:01 -0700

    fetch data

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-console.log("Hi there!");
-// need to fetch movie data from an API omdbapi.com index vs show api call 
-// autocomplete functionality - need rules for how it will work 
+// need to fetch movie data from an API omdbapi.com index vs show api call
+//  http://www.omdbapi.com/
+
+// Send all data requests to:
+// http://www.omdbapi.com/?apikey=[yourkey]&
+
+// autocomplete functionality - need rules for how it will work
 // style the app
+
+// make network request using axios
+// helper function
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "trilogy",
+      s: "avengers"
+    }
+  });
+  console.log(response.data);
+};
+fetchData();
