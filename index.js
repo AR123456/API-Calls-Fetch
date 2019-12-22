e3667d36acc9e70f71ff07add4c3862fe94013b4
AR123456 <coloradoz99@gmail.com>
2019-12-22 06:58:42 -0700

    compare functionality

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Hi there!');
+console.log("Hi there!");
+// need to fetch movie data from an API omdbapi.com index vs show api call 
+// autocomplete functionality - need rules for how it will work 
+// style the app
