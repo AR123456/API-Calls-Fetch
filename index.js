ab0f6953c34239006e27322d897f35f38829be95
AR123456 <coloradoz99@gmail.com>
2019-12-23 07:54:15 -0700

    compare search on input change

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-// need to fetch movie data from an API omdbapi.com index vs show api call
-//  http://www.omdbapi.com/
-
-// Send all data requests to:
-// http://www.omdbapi.com/?apikey=[yourkey]&
-
-// make network request using axios
-// helper function
-const fetchData = async () => {
+const fetchData = async searchTerm => {
   // the index get request
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "trilogy",
-      // s: "avengers"
-      // changing to I and hard coding a move to make the data request about a specific movie
-      i: "tt0848228"
+      s: searchTerm
     }
   });
   console.log(response.data);
 };
-fetchData();
 
-// autocomplete functionality - need rules for how it will work
-// input box where user types in search term
-// search with each letter entered
-// when finished typeing show results
-// if no movie found do somenthing to indicate that
-// ignore the enter key
-// click on the movie display picks it
+const input = document.querySelector("input");
 
-// style the app
+input.addEventListener("input", event => {
+  fetchData(event.target.value);
+});
