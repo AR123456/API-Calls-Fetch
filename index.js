0823be32d7e2454a15914227a100df01c7fd2278
AR123456 <coloradoz99@gmail.com>
2019-12-24 04:34:13 -0700

    extract utils into own file

--- index.js
+++ index.js
@@ -10,18 +10,7 @@ const fetchData = async searchTerm => {
 };
 
 const input = document.querySelector("input");
-// this delay is the default , it is being over ridden when 500 is passed into the eventListener below
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
+ 
 
 const onInput = event => {
   fetchData(event.target.value);
