ab1790a88fe32e51f7684a33f93fd2d07e800ce2
AR123456 <coloradoz99@gmail.com>
2019-12-24 04:35:24 -0700

    render in dom

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ const fetchData = async searchTerm => {
 const input = document.querySelector("input");
 
 const onInput = async event => {
-  const movies = await fetchData(event.target.value);
-  console.log(movies);
+  const movie = await fetchData(event.target.value);
+  // using for of here to loop , this is not supported by IE at this time so beware
+  // could you another loop methond here
+
+  for (let movies of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+  <img src="${movie.Poster}"/>
+  <h1>${movie.Title}</h1>
+  `;
+    document.querySelector("#target").appendChild(div);
+  }
 };
 
 input.addEventListener("input", debounce(onInput, 500));
