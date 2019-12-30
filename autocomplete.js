43849c13100818f21daf93b613061fe6c9902889
AR123456 <coloradoz99@gmail.com>
2019-12-30 07:18:27 -0700

    make auto complete resuable

---
+++ autocomplete.js
@@ -0,0 +1,5 @@
+// function that will take the autocomplete configuraion and render the auto complete on the screen
+// fetchData() - finds stuff
+//renderOption()- function that knows how to render sufff
+// onOptionSelected() function that gets invoked when a user clicks an option
+// root - element that the autocomplete configuration should be rendered into
