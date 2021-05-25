81c2bc3e1f80042d7fa6c535921766084cdd2153
AR123456 <coloradoz99@gmail.com>
2021-05-25 07:02:06 -0600

    Expanding Cards Widget

---
+++ Expanding Cards Widget V1/app.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel");
+// loop the node list
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
