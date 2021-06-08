24fcd7f4a9c95a8a916c236866b961c479036af6
AR123456 <coloradoz99@gmail.com>
2021-06-08 06:43:25 -0600

    Random Choice From Array

---
+++ Random Choice/app.js
@@ -0,0 +1,37 @@
+const tagsEL = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  console.log(input);
+  //   split on common
+  const tags = input
+    .split(",")
+    // remove white space
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  console.log(tags);
+  // clear the text area
+  tagsEL.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEL.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  //
+}
