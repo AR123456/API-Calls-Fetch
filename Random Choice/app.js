995421912ae2722a1eda482bfcf85fde22421166
AR123456 <coloradoz99@gmail.com>
2021-06-09 06:22:46 -0600

    Random choice generator

--- Random Choice/app.js
+++ Random Choice/app.js
@@ -1,38 +1,39 @@
-const tagsEL = document.getElementById("tags");
+const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
 
 textarea.focus();
+
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
   if (e.key === "Enter") {
     setTimeout(() => {
       e.target.value = "";
     }, 10);
+
     randomSelect();
   }
 });
 
 function createTags(input) {
-  //   split on common
   const tags = input
     .split(",")
-    // remove white space
     .filter((tag) => tag.trim() !== "")
     .map((tag) => tag.trim());
 
-  // clear the text area
-  tagsEL.innerHTML = "";
+  tagsEl.innerHTML = "";
 
   tags.forEach((tag) => {
     const tagEl = document.createElement("span");
     tagEl.classList.add("tag");
     tagEl.innerText = tag;
-    tagsEL.appendChild(tagEl);
+    tagsEl.appendChild(tagEl);
   });
 }
 
 function randomSelect() {
   const times = 30;
+
   const interval = setInterval(() => {
     const randomTag = pickRandomTag();
 
@@ -41,21 +42,28 @@ function randomSelect() {
     setTimeout(() => {
       unHighlightTag(randomTag);
     }, 100);
-    setTimeout(() => {
-      clearInterval(interval);
-      setTimeout(() => {
-        const randomTag = pickRandomTag();
-      });
-    });
   }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
 }
+
 function pickRandomTag() {
   const tags = document.querySelectorAll(".tag");
   return tags[Math.floor(Math.random() * tags.length)];
 }
+
 function highlightTag(tag) {
   tag.classList.add("highlight");
 }
+
 function unHighlightTag(tag) {
   tag.classList.remove("highlight");
 }
