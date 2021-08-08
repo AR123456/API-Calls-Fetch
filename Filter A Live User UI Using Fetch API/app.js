638611de78354ea8b33ee1ac4e5e43f5a7860082
AR123456 <coloradoz99@gmail.com>
2021-08-08 08:26:31 -0600

    making fetch request

---
+++ Filter A Live User UI Using Fetch API/app.js
@@ -0,0 +1,31 @@
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
+const listItems = [];
+getData();
+//  using this to get pics https://randomuser.me/
+async function getData() {
+  const res = await fetch("https://randomuser.me/api/?results=50");
+  const { results } = await res.json();
+  //   const data = await res.json();
+  // waits until the request completes...
+  //   console.log(data);
+  // clear rusults out of the DOM
+  result.innerHTML = "";
+  //   loop through each user and make a list item
+  results.forEach((user) => {
+    const li = document.createElement("li");
+    // console.log(user);
+    //  push onto a list item
+    listItems.push(li);
+    li.innerHTML = `
+    <img src="${user.picture.large}" alt="${user.name.first}">
+    <div class="user-info">
+    <h4>${user.name.first} ${user.name.last}</h4>
+    <p>
+    ${user.location.city}, ${user.location.country}
+    </p>
+    </div>
+    `;
+    result.appendChild(li);
+  });
+}
