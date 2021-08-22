edd5a2c6dd613c1e7561339816e661510de6fdb2
AR123456 <coloradoz99@gmail.com>
2021-08-22 11:26:49 -0600

    Insect multiplier JS final

--- Insect Multiplyer/app.js
+++ Insect Multiplyer/app.js
@@ -1,6 +1,6 @@
 const screens = document.querySelectorAll(".screen");
 // screens is a node list of the 3 screens in the HTML
-console.log(screens);
+// console.log(screens);
 // used in css to shift the node list itmes up 100vh to come into view
 // screens[0].classList.add("up");
 const choose_insect_btns = document.querySelectorAll(".choose-insect-btn");
@@ -26,14 +26,51 @@ choose_insect_btns.forEach((btn) => {
     startGame();
   });
 });
+function startGame() {
+  setInterval(increaseTime, 1000);
+}
+function increaseTime() {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  timeEl.innerHTML = `Time: ${m}:${s}`;
+  seconds++;
+}
 function createInsect() {
   const insect = document.createElement("div");
   insect.classList.add("insect");
   const { x, y } = getRandomLocation();
+  insect.style.top = `${y}px`;
+  insect.style.left = `${x}px`;
+  insect.innerHTML = `<img src="${selected_insect.src}" alt="${
+    selected_insect.alt
+  }" style="transform: rotate(${Math.random() * 360} deg) " />`;
+  insect.addEventListener("click", catchInsect);
+  game_container.appendChild(insect);
 }
 function getRandomLocation() {
   const width = window.innerHeight;
   const height = window.innerHeight;
   const x = Math.random() * (width - 200) + 100;
   const y = Math.random() * (height - 200) + 100;
+  return { x, y };
+}
+function catchInsect() {
+  increaseScore();
+  //   insect clicked on
+  this.classList.add("caught");
+  setTimeout(() => this.remove(), 2000);
+  addInsects();
+}
+function addInsects() {
+  setTimeout(createInsect, 1000);
+  setTimeout(createInsect, 1500);
+}
+function increaseScore() {
+  score++;
+  if (score > 19) {
+    message.classList.add("visable");
+  }
+  scoreEl.innerHTML = `Score: ${score}`;
 }
