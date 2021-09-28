846760c86a9d1a873ab3cf8cbe6d2c9854095afa
AR123456 <coloradoz99@gmail.com>
2021-09-28 06:04:31 -0600

    color fliper generate random

--- Background-Color-flipper/app.js
+++ Background-Color-flipper/app.js
@@ -1,1 +1,17 @@
-const colors = ["green", "red", "rgba(133,133,200", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector("color");
+
+btn.addEventListener("click", function () {
+  // do some stuff - target body and change its color each time clicked
+  //   style backgound
+  //   console.log(document.body);
+  // loop array and get rand array entry
+  getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+function getRandomNumber() {
+  randomNumber = Math.floor(Math.random() * colors.length);
+  console.log(randomNumber);
+}
