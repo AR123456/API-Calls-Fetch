0063a9a26dc297f8e0375a1f98cb7a088f88c457
AR123456 <coloradoz99@gmail.com>
2021-09-28 06:03:05 -0600

    background color flipper set up on click

---
+++ Background-Color-flipper - v1 set up on click/app.js
@@ -0,0 +1,11 @@
+const colors = ["green", "red", "rgba(133,133,200", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector("color");
+
+btn.addEventListener("click", function () {
+  // do some stuff - target body and change its color each time clicked
+  //   style backgound
+  //   console.log(document.body);
+  // loop array and get rand array entry
+  const randomNumber = 2;
+});
