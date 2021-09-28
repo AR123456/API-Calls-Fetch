c33779ebb13fb2610977ebe1f61f21a37ae4bd04
AR123456 <coloradoz99@gmail.com>
2021-09-28 06:05:09 -0600

    color flipper update text with color name

--- Background-Color-flipper/app.js
+++ Background-Color-flipper/app.js
@@ -1,6 +1,6 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
-const color = document.querySelector("color");
+const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   // do some stuff - target body and change its color each time clicked
