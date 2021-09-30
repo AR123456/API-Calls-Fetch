f4ea1b6997d26f8f6105aa1c71b5fc95b59a6a95
AR123456 <coloradoz99@gmail.com>
2021-09-30 06:03:05 -0600

    using arrows and for each

--- Background-Color-flipper/hex.js
+++ Background-Color-flipper/hex.js
@@ -3,7 +3,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", function () {
   let hexColor = "#";
   // for length of hex array get a random indes value and concatonate onto hextcolor
   for (let i = 0; i < 6; i++) {
@@ -13,6 +13,6 @@ btn.addEventListener("click", () => {
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
 });
-getRandomNumber = () => {
+function getRandomNumber() {
   return Math.floor(Math.random() * hex.length);
-};
+}
