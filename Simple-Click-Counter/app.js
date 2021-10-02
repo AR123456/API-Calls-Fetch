47315df1d18845fffad5ba491b2145896b522ae3
AR123456 <coloradoz99@gmail.com>
2021-10-02 05:12:03 -0600

    click counter background changes

--- Simple-Click-Counter/app.js
+++ Simple-Click-Counter/app.js
@@ -1,6 +1,7 @@
 const decrease = document.querySelector(".decrease");
 const increase = document.querySelector(".increase");
 const reset = document.querySelector(".reset");
+const value = document.getElementById("value");
 
 count = 0;
 
@@ -8,13 +9,20 @@ decrease.addEventListener("click", () => {
   console.log("decrease ");
   count--;
   console.log(count);
-  document.getElementById("value").innerHTML = count;
+  value.innerHTML = count;
+  value.style.background = "red";
 });
 
 increase.addEventListener("click", () => {
   console.log("increase");
+  count++;
+  value.innerHTML = count;
+  value.style.background = "green";
 });
 
 reset.addEventListener("click", () => {
   console.log("reset");
+  count = 0;
+  value.innerHTML = count;
+  value.style.background = "transparent";
 });
