f4e750e86a53a872c759bd1934490f8c7236e05b
AR123456 <coloradoz99@gmail.com>
2021-10-13 06:15:51 -0600

    FAQ traversing the dom

--- FAQ-Reveal-onclick/app.js
+++ FAQ-Reveal-onclick/app.js
@@ -1,13 +1,22 @@
-const questions = document.querySelectorAll(".question");
+// const questions = document.querySelectorAll(".question");
 // loop the node list
-questions.forEach(function (question) {
-  const btn = question.querySelector(".question-btn");
-  btn.addEventListener("click", function () {
-    questions.forEach(function (item) {
-      if (item !== question) {
-        item.classList.remove("show-text");
-      }
-    });
+// questions.forEach(function (question) {
+//   const btn = question.querySelector(".question-btn");
+//   btn.addEventListener("click", function () {
+//     questions.forEach(function (item) {
+//       if (item !== question) {
+//         item.classList.remove("show-text");
+//       }
+//     });
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+// //////////////////////traversin the dom
+const btns = document.querySelectorAll(".question-btn");
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
     question.classList.toggle("show-text");
   });
 });
