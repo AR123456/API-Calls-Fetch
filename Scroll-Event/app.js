fc8402cd726917e5d30bda5214879e632fb378f9
AR123456 <coloradoz99@gmail.com>
2021-10-19 06:27:19 -0600

    scroll event find place on page

--- Scroll-Event/app.js
+++ Scroll-Event/app.js
@@ -1,2 +1,20 @@
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+// close links
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  //   console.log("clicked");
+  //   linksContainer.classList.toggle("show-links");
+  const linksHeight = links.getBoundingClientRect().height;
+  const containterHeight = linksContainer.getBoundingClientRect().height;
+  //   console.log(linksHeight, containterHeight);
+  if (containterHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+  console.log(linksContainer.getBoundingClientRect());
+});
